7dd4507128af9d911b7225d891233fdf52b92202
PewBaca <mt.dev.android@gmail.com>
2026-04-24 07:15:49 +0200

    Add files via upload

---
+++ AllerPaw/app/src/main/java/com/allerpaw/app/data/local/AppDatabase.kt
@@ -0,0 +1,41 @@
+package com.allerpaw.app.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.allerpaw.app.data.local.dao.*
+import com.allerpaw.app.data.local.entity.*
+
+@Database(
+    entities = [
+        HundEntity::class,
+        HundGewichtEntity::class,
+        ZutatEntity::class,
+        ZutatNaehrstoffEntity::class,
+        RezeptEntity::class,
+        RezeptZutatEntity::class,
+        ParameterEntity::class,
+        ToleranzEntity::class,
+        TagebuchUmweltEntity::class,
+        TagebuchSymptomEntity::class,
+        TagebuchFutterEntity::class,
+        TagebuchFutterItemEntity::class,
+        TagebuchAusschlussEntity::class,
+        TagebuchAllergenEntity::class,
+        TagebuchTierarztEntity::class,
+        TagebuchMedikamentEntity::class,
+        TagebuchPollenLogEntity::class,
+        AusschlussPhasEntity::class,
+        EigenePollenartEntity::class,
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun hundDao(): HundDao
+    abstract fun zutatenDao(): ZutatenDao
+    abstract fun rezeptDao(): RezeptDao
+    abstract fun tagebuchDao(): TagebuchDao
+    abstract fun parameterDao(): ParameterDao
+}
